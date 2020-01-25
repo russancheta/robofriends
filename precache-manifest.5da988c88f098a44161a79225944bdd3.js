70cd9baef1dd53f229d55d7099467c9b58916835
Russel Ancheta <russel13799@gmail.com>
2020-01-25 11:59:49 +0800

    Updates

---
+++ precache-manifest.5da988c88f098a44161a79225944bdd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a36174490746cb650282190982de08",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e80c600ad78460bf37e2",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "33f4759f22fa4f91baec",
+    "url": "/robofriends/static/css/main.fd64a23d.chunk.css"
+  },
+  {
+    "revision": "e80c600ad78460bf37e2",
+    "url": "/robofriends/static/js/2.d6fca685.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.d6fca685.chunk.js.LICENSE"
+  },
+  {
+    "revision": "33f4759f22fa4f91baec",
+    "url": "/robofriends/static/js/main.03966670.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
